eb28f2c1742d6ef1b1cf2429aabcf7fbfa7ed513
g6no <ga6aan@gmail.com>
2022-03-15 12:50:28 +0300

    Update lec2.py

--- lec2.py
+++ lec2.py
@@ -150,3 +150,91 @@
 # print('Hi! I\'m Eli.')
 # print('The title of the book was \\\"Good Omens\\\".')
 # print('Hi! I\\\'m Sebastien.')
+
+
+
+#####################                               Lecture 5 Python                                ######################
+
+# Magic numbers are unexplained numbers appearing in code
+# (amount = balance * 0.069) --> in this example, 0.069 is unknown to signify what
+
+# To avoid this, we use named constants, whose value does not change throughout the program
+# INTEREST_RATE = 0.069
+# Naming convention for named constants --> all upper case
+# amount = balance * INTEREST_RATE
+
+# if statements in python
+# if condition:
+#     statement
+# elif condition:
+#     statement
+# else:
+#     statement
+
+# Comparing strings:
+# string1 == (!=) string2 to check if the string equals or does not equal
+
+# comparison operators on strings: compare using >, <, >=, <=
+# compares characters using ASCII values, same as alphabetical order if same case
+
+# print('y'>'k') returns True
+# print('y'>'z') returns False
+
+# print('Max' > 'Last') returns True
+
+# min_salary = 30000.0
+# min_years = 2
+#
+# salary = float(input('Enter annual salary: '))
+# years_on_job = int(input('Enter years on job: '))
+#
+# if salary >= min_salary:
+#     if years_on_job >= min_years:
+#         print('qualify')
+#     else:
+#         print('More years')
+# else:
+#     print('You must earn at least $', format(min_salary, ',.2f'), ' per year to qualify', sep='')
+
+# output:
+# Enter annual salary: 20000
+# Enter years on job: 2
+# You must earn at least $30,000.00 per year to qualify
+
+# Memory:
+# min_salary -> 30000
+# min_years -> 2
+# salary -> 20000
+# years_on_job -> 2
+
+# score = float(input('Enter your score: '))
+#
+# if score >= 90:
+#     print('A')
+# elif score >= 80:
+#     print('B')
+# elif score >= 70:
+#     print('C')
+# elif score >= 60:
+#     print('D')
+# elif score >= 0:
+#     print('F')
+# else:
+#     print('Invalid')
+
+
+# # Using nested if statements
+# if score >= 90:
+#     print('A')
+# else:
+#     if score >= 80:
+#         print('B')
+#     else:
+#         if score >= 70:
+#             print('C')
+#         else:
+#             if score >= 60:
+#                 print('D')
+#             else:
+#                 print('F')
+
